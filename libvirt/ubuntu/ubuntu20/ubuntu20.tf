a164e40785862111f7c22a908151b56de9b891f2
Marshall Onsrud <marshall@theonsruds.com>
2021-03-30 19:38:17 -0500

    add ubuntu20

--- libvirt/ubuntu/ubuntu20/ubuntu20.tf
+++ libvirt/ubuntu/ubuntu20/ubuntu20.tf
@@ -15,32 +15,45 @@ provider "libvirt" {
   uri = "qemu+ssh://root@virty.onsrud.home/system"
 }
 
-resource "libvirt_volume" "ubuntu20-qcow2" {
-  name   = "ubuntu20.qcow2"
+resource "libvirt_volume" "os_image_ubuntu20" {
+  name   = "os_image_ubuntu20"
   source = "http://kickstart.onsrud.home/focal-server-cloudimg-amd64.img"
-  format = "qcow2"
+  #format = "qcow2"
+  pool           = "default"
 }
+
+
+resource "libvirt_volume" "os_image_ubuntu20_resized" {
+  name           = "os_image_ubuntu20_resized"
+  base_volume_id = libvirt_volume.os_image_ubuntu20.id
+  pool           = "default"
+  size           = 93613931520
+}
+
 
 data "template_file" "user_data" {
   template = file("${path.module}/user-data")
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name      = "ubunt20-commoninit.iso"
+  name      = "ubuntu20-commoninit.iso"
   user_data = data.template_file.user_data.rendered
+  pool  = "default"
 }
 
 resource "libvirt_domain" "db1" {
   name   = "ubuntu20"
-  memory = "2048"
-  vcpu   = 2
+  memory = "4096"
+  vcpu   = 4
+  qemu_agent = true
 
   network_interface {
-    macvtap = "eno1"
+    network_name = "bridge0"
+    wait_for_lease = true
   }
 
   disk {
-    volume_id = libvirt_volume.ubuntu20-qcow2.id
+    volume_id = libvirt_volume.os_image_ubuntu20_resized.id
   }
 
   cloudinit = libvirt_cloudinit_disk.commoninit.id
@@ -56,6 +69,11 @@ resource "libvirt_domain" "db1" {
     listen_type = "address"
     autoport    = true
   }
+}
+
+
+output "ipv4" {
+  value = libvirt_domain.db1.*.network_interface.0.addresses
 }
 
 
